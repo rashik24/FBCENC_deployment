830e5dd46e9bfc0b4cbed8fafc64d4085a1fc11f
rashik24 <rsiddiq2@ncsu.edu>
2025-01-06 20:19:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ Upload a CSV file, and it will be merged with a **pre-loaded reference file** fo
 """)
 
 # Load the static reference file
-static_file_path = "/Users/rsiddiq2/Downloads/FBCENC Geo 2.csv"  # Replace with your file path
+static_file_path = static_file_path = "FBCENC_Geo_2.csv"
+
 try:
     ref_df = pd.read_csv(static_file_path)
     st.write("### Reference File Preview:")
