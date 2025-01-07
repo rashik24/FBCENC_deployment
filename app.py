0335410265b046e527f48b0802cb661b01117a30
rashik24 <rsiddiq2@ncsu.edu>
2025-01-06 21:50:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from processed import processing
-
+from agency_days import processing_agency_days_and_hours
 # Title and description
 st.title("File Processing and Merging App")
 st.write("""
@@ -9,8 +9,7 @@ Upload a CSV file, and it will be merged with a **pre-loaded reference file** fo
 """)
 
 # Load the static reference file
-static_file_path = static_file_path = "FBCENC_Geo_2.csv"
-
+static_file_path = "/Users/rsiddiq2/Downloads/FBCENC Geo 2.csv"  # Replace with your file path
 try:
     ref_df = pd.read_csv(static_file_path)
     st.write("### Reference File Preview:")
@@ -32,7 +31,7 @@ if uploaded_file:
     
     # Merge the uploaded file with the static file
     #merged_df = pd.merge(out, ref_df, on=key, how="inner")  # Default inner join
-    final_df = out.merge(ref_df[['Latitude','Longitude','No.']], on="No.", how="inner")
+    final_df = out.merge(ref_df[['Latitude','Longitude','No.']], on="No.", how="left")
     # Example processing (modify this part as needed)
     #merged_df['Processed'] = merged_df.iloc[:, 1] * 2  # Example logic - replace with your own
 
@@ -42,10 +41,29 @@ if uploaded_file:
 
     # Download button for the processed file
     st.download_button(
-        label="Download Processed File",
+        label="Download Processed File For PowerBI",
         data=final_df.to_csv(index=False).encode('utf-8'),
-        file_name="processed_file.csv",
+        file_name="PowerBI_Input_file.csv",
         mime="text/csv"
     )
+
+    # Second Processing Method
+    st.write("### Processing Method 2")
+    out2 = processing_agency_days_and_hours(new_df)
+    #final_df2 = out2.merge(ref_df[['Latitude', 'Longitude', 'No.']], on="No.", how="inner")
+
+    # Display the second processed output
+    st.write("#### Processed Data (Method 2):")
+    st.write(out2.head())
+
+    # Download button for Method 2 output
+    st.download_button(
+        label="Download Processed File for Agency days and hours",
+        data=final_df2.to_csv(index=False).encode('utf-8'),
+        file_name="processed_file_agency_days.csv",
+        mime="text/csv"
+    )
+
+
 else:
     st.info("Please upload a CSV file to continue.")
